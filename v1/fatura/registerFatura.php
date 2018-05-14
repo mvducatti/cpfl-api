bfbd15c38f48eeee9a4613708a3c8c7d8441532e
Marcos Ducatti <mvducatti@gmail.com>
2018-05-14 00:07:33 -0300

    Terminar parte do medidor

--- v1/fatura/registerFatura.php
+++ v1/fatura/registerFatura.php
@@ -1,34 +1,30 @@
-    <?php 
-     
-    require_once '../../includes/DBNewsOperations.php';
-     
-    $response = array(); 
-     
-    if($_SERVER['REQUEST_METHOD']=='POST'){
+<?php 
 
-    $news_post = $_POST['news_post'];
-    $user_FK = $_POST['user_FK'];
+require_once '../../includes/DBUserOperations.php';
 
-        if(empty($news_post) || empty($user_FK)){
+$response = array(); 
+
+if($_SERVER['REQUEST_METHOD']=='POST'){
+
+    if(empty($_POST['cpf']) || empty($_POST['consumo'])){
         $response['error'] = true; 
         $response['message'] = "Por favor preencha todos os campos";
-    } else {
-
-        $db = new DBNewsOperations(); 
-     
-            $result = $db->registerNews($_POST['news_post'], $_POST['user_FK']);
-            if($result == 1 ){
-                $response['error'] = false; 
-                $response['message'] = "Noticia Registrada com sucesso";
-            }elseif($result == 2){
-                $response['error'] = true; 
-                $response['message'] = "Some error occurred please try again";          
-            }
-    }
-     
+        
     }else{
-        $response['error'] = true; 
-        $response['message'] = "Invalid Request";
+
+        $db = new DBUserOperations(); 
+
+        if($db->userLogin($_POST['year'],  $_POST['month'], $_POST['cpf'], $_POST['consumo'])){
+            $user = $db->getUserByCpf($_POST['cpf']);
+            $response['error'] = false; 
+            $response['cpf'] = $user['cpf'];
+            $response['nome'] = $user['username'];
+            $response['message'] = "Fatura gerada com sucesso!"; 
+        }else{
+            $response['error'] = true; 
+            $response['message'] = "CPF Inexistente, verifique novamente";          
+        }
     }
-     
-    echo json_encode($response);
+}
+
+echo json_encode($response);
